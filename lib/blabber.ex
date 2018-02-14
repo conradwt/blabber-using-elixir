c2684c750d8797a7124d663eec4bc8ee7cbd392e
Conrad Taylor <conradwt@gmail.com>
2018-02-14 01:21:39 -0800

    Code cleanup.

--- lib/blabber.ex
+++ lib/blabber.ex
@@ -1,17 +1,17 @@
 defmodule Blabber do
-  
   def start do
-    spawn( __MODULE__, :loop, [1] )
-  end
-  
-  def loop( count ) do
-    receive do
-      :done -> IO.puts( "Closing" )
-              :ok
-    after 
-      2000 -> IO.puts( "Loop #{node()} with #{count} says hello" )
-              Blabber.loop( count + 1 ) 
-    end
+    spawn(__MODULE__, :loop, [1])
   end
 
+  def loop(count) do
+    receive do
+      :done ->
+        IO.puts("Closing")
+        :ok
+    after
+      2000 ->
+        IO.puts("Loop #{node()} with #{count} says hello")
+        Blabber.loop(count + 1)
+    end
+  end
 end
