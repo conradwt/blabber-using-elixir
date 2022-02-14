8c213d109a67a02cf4ac12b6650768c860fa5d88
Conrad Taylor <conradwt@gmail.com>
2022-02-13 20:51:51 -0800

    Upgrade to Elixir 1.13.3.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blabber.Mixfile do
     [
       app: :blabber,
       version: "0.1.0",
-      elixir: "~> 1.13.2",
+      elixir: "~> 1.13.3",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
